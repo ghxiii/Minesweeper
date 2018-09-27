bcb2193231670e5cbb18d62c893e16f68f49ed7b
skun21041 <steffen.kunert@kaufland.com>
2018-09-27 10:43:35 +0200

    Initial commit

---
+++ src/Main/GUI.java
@@ -0,0 +1,24 @@
+package Main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI {
+    int fieldSizeX;
+    int fieldSizeY;
+
+    GUI(int fieldSizeX, int fieldSizeY) {
+        this.fieldSizeX = fieldSizeX;
+        this.fieldSizeY = fieldSizeY;
+    }
+
+    public void start() {
+        JFrame frame = new JFrame("Minesweeper");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension dim =  new Dimension(400, 400);
+        frame.setMinimumSize(dim);
+        JTable field = new JTable(fieldSizeX, fieldSizeY);
+        frame.add(field);
+        frame.setVisible(true);
+    }
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GUI gui = new GUI(20, 20);
+        gui.start();
+    }
+}
