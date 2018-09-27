87e6d75c2f664aae9d228d9e2b141aaa5da5e4a8
skun21041 <steffen.kunert@kaufland.com>
2018-09-27 14:52:36 +0200

    Add MinesweeperField as attribute

--- src/Main/GUI.java
+++ src/Main/GUI.java
@@ -1,29 +1,34 @@
 package Main;
 
+import MinesweeperBackend.MinesweeperField;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GUI {
     JFrame frame;
     JTable GameField;
+    MinesweeperField msf;
 
     GUI(int fieldSizeX, int fieldSizeY) {
         this.frame = new JFrame("Minesweeper");
         this.GameField = new JTable(fieldSizeX, fieldSizeY);
+        this.msf = new MinesweeperField(fieldSizeX, fieldSizeY, 15);
     }
 
     public void start() {
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Dimension dim = new Dimension(400, 400);
-        this.frame.setMinimumSize(dim);
+        Dimension dim = new Dimension(400, 360);
         int[][] fieldArray = new int[][]{{2, 4}, {1, 3}}; // DEBUG ONLY
         this.updateField(fieldArray);
         this.frame.add(this.GameField);
+        this.frame.setPreferredSize(dim);
+        this.frame.pack();
         this.frame.setVisible(true);
         this.setupClickHandler();
     }
 
-    public void updateField(int[][] field) {
+    private void updateField(int[][] field) {
         int xLength = field.length;
         int yLength = field[0].length;
 
